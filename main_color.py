cd4cf974cca68ef5f4907ce680d91be1d47572a6
Tom White <tom@sixdozen.com>
2017-05-08 11:32:57 +1200

    Quick hack to get this to work on my flags dataset

--- main_color.py
+++ main_color.py
@@ -10,29 +10,36 @@ import matplotlib.pylab as mp
 from PIL import Image
 import numpy
 import pdb
-ino = 16
-jno = 20
-psize = 40
+import glob
+
+ino = 17
+jno = 17
+ysize = 128
+xsize = 224
 iname = 'images/img'
 
 imgdata = []
 data = []
 counter = 0
-for i in range(ino):
-    for j in range(jno):
-        counter = counter + 1
-        fname = iname + str(counter) + '.jpg'
-        im = Image.open(fname)
-        aim = numpy.asarray(im)
-        [M,N,L] = aim.shape
-        mno = int(M/psize)
-        nno = int(N/psize)
-        aim = aim[0:psize*mno:mno,0:psize*nno:nno,:]
-        data.append(aim.flatten())
-        daim = numpy.double(aim)/255.0
-        # convert from RGB to Lab
-        daimlab = lab(daim)
-        imgdata.append(daimlab.flatten())
+images = glob.glob("/develop/data/region-flags/png/close/*.png")
+
+# for i in range(ino):
+#     for j in range(jno):
+for fname in images:
+    # counter = counter + 1
+    # fname = iname + str(counter) + '.jpg'
+    im = Image.open(fname).convert('RGB')
+    aim = numpy.asarray(im)
+    print(aim.shape)
+    [M,N,L] = aim.shape
+    mno = int(M/ysize)
+    nno = int(N/xsize)
+    aim = aim[0:ysize*mno:mno,0:xsize*nno:nno,:]
+    data.append(aim.flatten())
+    daim = numpy.double(aim)/255.0
+    # convert from RGB to Lab
+    daimlab = lab(daim)
+    imgdata.append(daimlab.flatten())
         
 data = numpy.array(data)
 imgdata = numpy.array(imgdata)
@@ -44,12 +51,12 @@ griddata[1,] = numpy.tile(range(1,jno+1),(1,ino))
 PI = KS(imgdata,griddata.T)
 i_sorting = PI.argmax(axis=1)
 imgdata_sorted = data[i_sorting,]
-irange = range(0,psize*ino,psize)
-jrange = range(0,psize*jno,psize)
-patching = numpy.zeros((ino*psize, jno*psize, 3))
+irange = range(0,ysize*ino,ysize)
+jrange = range(0,xsize*jno,xsize)
+patching = numpy.zeros((ino*ysize, jno*xsize, 3))
 for i in range(ino):
     for j in range(jno):
-        patching[irange[i]:irange[i]+psize,jrange[j]:jrange[j]+psize,:] = numpy.reshape(imgdata_sorted[(i)*jno+j,], [psize,psize,3]);
+        patching[irange[i]:irange[i]+ysize,jrange[j]:jrange[j]+xsize,:] = numpy.reshape(imgdata_sorted[(i)*jno+j,], [ysize,xsize,3]);
 
 im = Image.fromarray(patching.astype(numpy.uint8))
 # im.show()
